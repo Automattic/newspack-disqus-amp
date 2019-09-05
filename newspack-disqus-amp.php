768cc8d4b978ba010a4d11f098a2547e65b5ce28
claudiulodro <claudiulodro@gmail.com>
2019-09-05 14:39:30 -0700

    Clean up main plugin file

--- newspack-disqus-amp.php
+++ newspack-disqus-amp.php
@@ -12,8 +12,14 @@
 
 defined( 'ABSPATH' ) || exit;
 
+/**
+ * Manages the whole show.
+ */
 class Newspack_Disqus_AMP {
 
+	/**
+	 * Initialize everything if Disqus is active.
+	 */
 	public static function init() {
 		if ( ! function_exists( 'is_plugin_active' ) ) {
 			include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
@@ -27,6 +33,12 @@ class Newspack_Disqus_AMP {
 		add_filter( 'dsq_can_load', [ __CLASS__, 'dont_load_disqus_scripts_on_amp' ] );
 	}
 
+	/**
+	 * Don't enqueue Disqus scripts on AMP pages.
+	 *
+	 * @param string $script The name of the script Disqus is trying to endqueue.
+	 * @return string|bool Return false when scripts shouldn't be enqueued.
+	 */
 	public static function dont_load_disqus_scripts_on_amp( $script ) {
 		if ( function_exists( 'is_amp_endpoint' ) && is_amp_endpoint() ) {
 			return false;
@@ -34,6 +46,12 @@ class Newspack_Disqus_AMP {
 		return $script;
 	}
 
+	/**
+	 * On AMP pages, replace the default comment template with an AMP-compatible Disqus comment template.
+	 *
+	 * @param string $template Comment template path.
+	 * @return string Comment template path.
+	 */
 	public static function render_comments( $template ) {
 		if ( ! comments_open() || ! is_singular() || ! function_exists( 'is_amp_endpoint' ) || ! is_amp_endpoint() ) {
 			return $template;
@@ -41,6 +59,5 @@ class Newspack_Disqus_AMP {
 
 		return dirname( __FILE__ ) . '/comments-template.php';
 	}
-
 }
 Newspack_Disqus_AMP::init();
