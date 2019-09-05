bfd5f2319ced3a204f979a39b0930cdf7dc60c6b
claudiulodro <claudiulodro@gmail.com>
2019-09-05 12:12:13 -0700

    rawgit is deprecated

--- comments-template.php
+++ comments-template.php
@@ -13,7 +13,7 @@ $page_url = get_permalink(); // @todo urlencode and decode on the other end.
 	sandbox="allow-scripts allow-same-origin allow-modals allow-popups allow-forms"
 	resizable
 	frameborder="0"
-	src="https://cdn.rawgit.com/automattic/newspack-disqus-amp/master/comment-loader.html?shortname=<?php echo $shortname; ?>&url=<?php echo $page_url; ?>"
+	src="https://automattic.github.io/newspack-disqus-amp/comment-loader.html?shortname=<?php echo $shortname; ?>&url=<?php echo $page_url; ?>"
 >
 	<div overflow tabindex=0 role=button aria-label="<?php echo __( 'Comments' ); ?>"></div>
 </amp-iframe>
